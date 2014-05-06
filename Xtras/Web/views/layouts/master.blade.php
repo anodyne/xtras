72e49d14b73a0b5512e1f37b9304c8fde31f243a
David VanScott <david.vanscott@gmail.com>
2014-05-06 10:37:51 -0400

    Master layout work

--- Xtras/Web/views/layouts/master.blade.php
+++ Xtras/Web/views/layouts/master.blade.php
@@ -3,14 +3,28 @@
 	<head>
 		<meta charset="utf-8">
 		<title>AnodyneXtras &bull; @yield('title')</title>
+		<meta name="description" content="AnodyneXtras is a one-stop-shop for skins, MODs, and rank sets for Anodyne Productions' Nova software.">
+		<meta name="author" content="Anodyne Productions">
+		<meta name="viewport" content="width=device-width">
+		<link rel="icon" type="image/x-icon" href="{{ URL::asset('favicon.ico?v1') }}">
+		<link rel="apple-touch-icon-precomposed" href="{{ URL::asset('apple-touch-icon.png') }}">
 
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-		<link href="http://fonts.googleapis.com/css?family=Bitter:400,700" rel="stylesheet">
-		<link href="http://fonts.googleapis.com/css?family=Exo+2:500,500italic,600,600italic" rel="stylesheet">
+		<!--[if lt IE 9]>
+		{{ HTML::script('js/html5shiv.js') }}
+		<![endif]-->
 
-		<link rel="stylesheet" href="http://localhost/global/bootstrap/3.1/css/bootstrap.min.css">
+		@if (App::environment() == 'production')
+			<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+			<link href="http://fonts.googleapis.com/css?family=Bitter:400,700" rel="stylesheet">
+			<link href="http://fonts.googleapis.com/css?family=Exo+2:500,500italic,600,600italic" rel="stylesheet">
+			<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+		@else
+			<link href="http://localhost/global/bootstrap/3.1/css/bootstrap.min.css" rel="stylesheet">
+		@endif
+
 		{{ HTML::style('css/style.css') }}
 		{{ HTML::style('css/fonts.css') }}
+		@yield('styles')
 	</head>
 	<body>
 		<nav class="nav-main">
@@ -63,6 +77,10 @@
 
 		<section>
 			<div class="container">
+				@if (Session::has('flashMessage'))
+					{{ partial('alert', ['type' => Session::get('flashStatus'), 'content' => Session::get('flashMessage')]) }}
+				@endif
+
 				@yield('content')
 			</div>
 		</section>

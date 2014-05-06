acc9d9c587a8b196737308b0250dc9e6f3fd2f39
David VanScott <david.vanscott@gmail.com>
2014-05-06 09:36:00 -0400

    Add the API structure

---
+++ Xtras/Api/XtrasApiServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Xtras\Api;
+
+use Illuminate\Support\ServiceProvider;
+
+class XtrasApiServiceProvider extends ServiceProvider {
+
+	public function register()
+	{
+		//
+	}
+
+	public function boot()
+	{
+		//
+	}
+
+}

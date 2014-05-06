38925060868ec6de66e5fa99c4a64c5180284044
David VanScott <david.vanscott@gmail.com>
2014-05-06 13:50:42 -0400

    Work on the item view page

--- Xtras/Web/views/pages/items/show.blade.php
+++ Xtras/Web/views/pages/items/show.blade.php
@@ -11,55 +11,74 @@
 
 	<div>{{ $item->present()->description }}</div>
 
-	<p>{{ $item->present()->downloadBtn }}</p>
+	<div class="visible-md visible-lg">
+		{{ partial('btn-toolbar', ['data' => [0 => [['link' => '#', 'text' => 'Download the Latest Version', 'class' => 'btn btn-lg btn-primary']]]]) }}
+	</div>
 
 	<div class="row">
 		<div class="col-lg-9">
-			<h2>Details</h2>
+			<ul class="nav nav-tabs">
+				<li class="active"><a href="#details" data-toggle="tab">Details</a></li>
 
-			<dl>
-				<dt>Author</dt>
-				<dd>{{ $item->present()->author }}</dd>
-
-				<dt>Downloads</dt>
-				<dd>{{ $item->present()->downloads }}</dd>
-
-				@if ($item->present()->rating !== false)
-					<dt>Rating</dt>
-					<dd>{{ $item->present()->rating }}</dd>
+				@if ($item->meta and ! empty($item->meta->installation))
+					<li><a href="#install" data-toggle="tab">Installation</a></li>
 				@endif
 
-				<dt>Added</dt>
-				<dd>{{ $item->present()->created }}</dd>
+				<li><a href="#versions" data-toggle="tab">Version History</a></li>
+				<li class="visible-md visible-lg"><a href="#download" data-toggle="tab">Downloads</a></li>
+				<li><a href="#comments" data-toggle="tab">Comments {{ $item->present()->commentsCount }}</a></li>
+			</ul>
 
-				@if ($item->present()->created != $item->present()->updated)
-					<dt>Last Updated</dt>
-					<dd>{{ $item->present()->updated }}</dd>
-				@endif
-			</dl>
+			<div class="tab-content">
+				<div id="details" class="active tab-pane">
+					<h2>Details</h2>
 
-			<div class="btn-toolbar">
-				<div class="btn-group">
-					<a href="#" class="btn btn-default">Report an Issue</a>
+					<dl>
+						<dt>Author</dt>
+						<dd>{{ $item->present()->author }}</dd>
+
+						<dt>Downloads</dt>
+						<dd>{{ $item->present()->downloads }}</dd>
+
+						@if ($item->present()->rating !== false)
+							<dt>Rating</dt>
+							<dd>{{ $item->present()->rating }}</dd>
+						@endif
+
+						<dt>Added</dt>
+						<dd>{{ $item->present()->created }}</dd>
+
+						@if ($item->present()->created != $item->present()->updated)
+							<dt>Last Updated</dt>
+							<dd>{{ $item->present()->updated }}</dd>
+						@endif
+					</dl>
+
+					{{ partial('btn-toolbar', ['data' => [0 => [['link' => '#', 'text' => 'Report an Issue', 'class' => 'btn btn-default']], 1 => [['link' => '#', 'text' => 'Report Abuse to Anodyne', 'class' => 'btn btn-default']]]]) }}
 				</div>
-				<div class="btn-group">
-					<a href="#" class="btn btn-default">Report Abuse to Anodyne</a>
+
+				@if ($item->meta and ! empty($item->meta->installation))
+					<div id="install" class="tab-pane">
+						<h2>Installation</h2>
+
+						{{ $item->meta->present()->installation }}
+					</div>
+				@endif
+
+				<div id="versions" class="tab-pane">
+					<h2>Version History</h2>
+				</div>
+
+				<div id="download" class="tab-pane">
+					<h2>Downloads</h2>
+				</div>
+
+				<div id="comments" class="tab-pane">
+					<h2>Comments</h2>
+
+					{{ partial('btn-toolbar', ['data' => [0 => [['link' => '#', 'text' => 'Add a Comment', 'class' => 'btn btn-default']]]]) }}
 				</div>
 			</div>
-
-			@if ($item->meta and ! empty($item->meta->installation))
-				<h2>Installation</h2>
-
-				{{ $item->meta->present()->installation }}
-			@endif
-
-			<h2>Version History</h2>
-
-			<h2>Downloads</h2>
-
-			@if ($item->comments->count() > 0)
-				<h2>Comments</h2>
-			@endif
 		</div>
 
 		<div class="col-lg-3">

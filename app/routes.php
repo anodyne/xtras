5b13e9a2ae174a7b6ac472f56fd2f148dad7d03f
David VanScott <david.vanscott@gmail.com>
2014-05-18 21:07:07 -0400

    Update the routes file

--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,5 @@
 
 Route::get('test', function()
 {
-	$filesystem = App::make('xtras.filesystem');
-
-	s($filesystem->put('foo.txt', "This is the content for my first file"));
-	s($filesystem->has('foo.txt'));
+	echo Markdown::parse('# Hello Parsedown');
 });

ca7959dc4ab969606ab80a045e75731ec893ee56
David VanScott <david.vanscott@gmail.com>
2014-05-24 13:42:15 -0400

    Update the layout and add the mobile page

--- Xtras/Web/views/layouts/master.blade.php
+++ Xtras/Web/views/layouts/master.blade.php
@@ -27,132 +27,137 @@
 		@yield('styles')
 	</head>
 	<body>
-		<nav class="nav-main">
-			<div class="container">
-				<ul class="pull-right">
-					<li><a href="#">Contact</a></li>
+		<div class="visible-xs visible-sm">
+			{{ View::make('pages.mobile') }}
+		</div>
+		<div class="visible-md visible-lg">
+			<nav class="nav-main">
+				<div class="container">
+					<ul class="pull-right">
+						<li><a href="#">Contact</a></li>
 
-					@if (Auth::check())
-						<li class="dropdown">
-							<a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="icn-size-16 user-icon">{{ $_icons['user'] }}</span> {{ Auth::user()->present()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu dropdown-menu-right dd">
-								<li><a href="{{ URL::route('xtras', [$_currentUser->slug]) }}">My Xtras</a></li>
-								<li><a href="{{ URL::route('xtra.create') }}">Create New Xtra</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ URL::route('profile', [$_currentUser->slug]) }}">My Profile</a></li>
-								<li><a href="{{ URL::route('account', [$_currentUser->slug]) }}">Edit My Profile</a></li>
-								<li class="divider"></li>
-								<li><a href="{{ URL::route('logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-
-				<ul>
-					<li><a href="http://anodyne-productions.com">Anodyne<div class="arrow"></div></a></li>
-					<li><a href="http://anodyne-productions.com/nova">Nova<div class="arrow"></div></a></li>
-					<li><a href="{{ URL::route('home') }}" class="active">Xtras<div class="arrow"></div></a></li>
-					<li><a href="http://forums.anodyne-productions.com">Forums<div class="arrow"></div></a></li>
-					<li><a href="http://help.anodyne-productions.com">Help<div class="arrow"></div></a></li>
-					<li><a href="http://learn.anodyne-productions.com">Learn<div class="arrow"></div></a></li>
-				</ul>
-			</div>
-		</nav>
-
-		<header>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-3 col-lg-3">
-						<a href="{{ URL::route('home') }}" class="brand">AnodyneXtras</a>
-					</div>
-
-					@if (Auth::check())
-						<div class="col-md-5 col-lg-5">
-							<nav class="nav-sub">
-								<ul>
-									<li><a href="#">Skins</a></li>
-									<li><a href="#">MODs</a></li>
-									<li><a href="#">Ranks</a></li>
-									<li><a href="{{ URL::route('xtras') }}">My Xtras</a></li>
+						@if (Auth::check())
+							<li class="dropdown">
+								<a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="icn-size-16 user-icon">{{ $_icons['user'] }}</span> {{ Auth::user()->present()->name }} <span class="caret"></span></a>
+								<ul class="dropdown-menu dropdown-menu-right dd">
+									<li><a href="{{ URL::route('xtras', [$_currentUser->slug]) }}">My Xtras</a></li>
+									<li><a href="{{ URL::route('xtra.create') }}">Create New Xtra</a></li>
+									<li class="divider"></li>
+									<li><a href="{{ URL::route('profile', [$_currentUser->slug]) }}">My Profile</a></li>
+									<li><a href="{{ URL::route('account', [$_currentUser->slug]) }}">Edit My Profile</a></li>
+									<li class="divider"></li>
+									<li><a href="{{ URL::route('logout') }}">Logout</a></li>
 								</ul>
-							</nav>
+							</li>
+						@endif
+					</ul>
+
+					<ul>
+						<li><a href="http://anodyne-productions.com">Anodyne<div class="arrow"></div></a></li>
+						<li><a href="http://anodyne-productions.com/nova">Nova<div class="arrow"></div></a></li>
+						<li><a href="{{ URL::route('home') }}" class="active">Xtras<div class="arrow"></div></a></li>
+						<li><a href="http://forums.anodyne-productions.com">Forums<div class="arrow"></div></a></li>
+						<li><a href="http://help.anodyne-productions.com">Help<div class="arrow"></div></a></li>
+						<li><a href="http://learn.anodyne-productions.com">Learn<div class="arrow"></div></a></li>
+					</ul>
+				</div>
+			</nav>
+
+			<header>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-lg-3">
+							<a href="{{ URL::route('home') }}" class="brand">AnodyneXtras</a>
 						</div>
 
-						<div class="col-md-4 col-lg-4">
-							<div class="header-search">
-								<div class="input-group">
-									{{ Form::text('search', null, array('placeholder' => 'Search Xtras', 'class' => 'input-sm form-control search-field')) }}
-									<span class="input-group-btn">{{ Form::button('Search', array('class' => 'btn btn-default btn-sm')) }}</span>
+						@if (Auth::check())
+							<div class="col-md-5 col-lg-5">
+								<nav class="nav-sub">
+									<ul>
+										<li><a href="#">Skins</a></li>
+										<li><a href="#">MODs</a></li>
+										<li><a href="#">Ranks</a></li>
+										<li><a href="{{ URL::route('xtras') }}">My Xtras</a></li>
+									</ul>
+								</nav>
+							</div>
+
+							<div class="col-md-4 col-lg-4">
+								<div class="header-search">
+									<div class="input-group">
+										{{ Form::text('search', null, array('placeholder' => 'Search Xtras', 'class' => 'input-sm form-control search-field')) }}
+										<span class="input-group-btn">{{ Form::button('Search', array('class' => 'btn btn-default btn-sm')) }}</span>
+									</div>
 								</div>
 							</div>
-						</div>
+						@endif
+					</div>
+				</div>
+			</header>
+
+			<section>
+				<div class="container">
+					@if (Session::has('flashMessage'))
+						{{ partial('alert', ['type' => Session::get('flashStatus'), 'content' => Session::get('flashMessage')]) }}
 					@endif
+
+					@yield('content')
 				</div>
-			</div>
-		</header>
+			</section>
 
-		<section>
-			<div class="container">
-				@if (Session::has('flashMessage'))
-					{{ partial('alert', ['type' => Session::get('flashStatus'), 'content' => Session::get('flashMessage')]) }}
-				@endif
+			<footer>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-lg-8">
+							<h2>AnodyneXtras</h2>
 
-				@yield('content')
-			</div>
-		</section>
+							<p class="text-muted">Every game is unique in its own right. It has its own players, characters, mission, and driving force. Why shouldn't each game be able to accurately reflect its distinctiveness through its look and feel and functionality? We don't think that should be a hinderance and we've created AnodyneXtras as a one-stop-shop for skins, MODs, and rank sets for Nova so you can make your copy of Nova as unique as the game being played on it.</p>
 
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-lg-8">
-						<h2>AnodyneXtras</h2>
+							<p class="text-muted">&copy; {{ Date::now()->year }} Anodyne Productions</p>
+						</div>
+						<div class="col-md-3 col-lg-2">
+							<ul class="list-unstyled">
+								<li><a href="{{ URL::route('home') }}">Home</a></li>
 
-						<p class="text-muted">Every game is unique in its own right. It has its own players, characters, mission, and driving force. Why shouldn't each game be able to accurately reflect its distinctiveness through its look and feel and functionality? We don't think that should be a hinderance and we've created AnodyneXtras as a one-stop-shop for skins, MODs, and rank sets for Nova so you can make your copy of Nova as unique as the game being played on it.</p>
-
-						<p class="text-muted">&copy; {{ Date::now()->year }} Anodyne Productions</p>
-					</div>
-					<div class="col-md-3 col-lg-2">
-						<ul class="list-unstyled">
-							<li><a href="{{ URL::route('home') }}">Home</a></li>
-
-							@if (Auth::check())
-								<li><a href="{{ URL::route('xtras') }}">My Xtras</a></li>
-								<li><a href="{{ URL::route('skins') }}">Skins</a></li>
-								<li><a href="{{ URL::route('ranks') }}">Ranks</a></li>
-								<li><a href="{{ URL::route('mods') }}">MODs</a></li>
-							@endif
-						</ul>
-					</div>
-					<div class="col-md-3 col-lg-2">
-						<ul class="list-unstyled">
-							<li><a href="{{ URL::route('policies') }}">Site Policies</a></li>
-							<li><a href="#">Contact</a></li>
-							<li><a href="http://anodyne-productions.com">Anodyne Productions</a></li>
-						</ul>
+								@if (Auth::check())
+									<li><a href="{{ URL::route('xtras') }}">My Xtras</a></li>
+									<li><a href="{{ URL::route('skins') }}">Skins</a></li>
+									<li><a href="{{ URL::route('ranks') }}">Ranks</a></li>
+									<li><a href="{{ URL::route('mods') }}">MODs</a></li>
+								@endif
+							</ul>
+						</div>
+						<div class="col-md-3 col-lg-2">
+							<ul class="list-unstyled">
+								<li><a href="{{ URL::route('policies') }}">Site Policies</a></li>
+								<li><a href="#">Contact</a></li>
+								<li><a href="http://anodyne-productions.com">Anodyne Productions</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
+			</footer>
 
-		@if (App::environment() == 'production')
-			<!--[if lt IE 9]>
-				<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-			<![endif]-->
-			<!--[if gte IE 9]><!-->
-				<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-			<!--<![endif]-->
+			@if (App::environment() == 'production')
+				<!--[if lt IE 9]>
+					<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+				<![endif]-->
+				<!--[if gte IE 9]><!-->
+					<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+				<!--<![endif]-->
 
-			<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-		@else
-			<!--[if lt IE 9]>
-				<script src="http://localhost/global/jquery/jquery-1.11.1.min.js"></script>
-			<![endif]-->
-			<!--[if gte IE 9]><!-->
-				<script src="http://localhost/global/jquery/jquery-2.1.1.min.js"></script>
-			<!--<![endif]-->
+				<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+			@else
+				<!--[if lt IE 9]>
+					<script src="http://localhost/global/jquery/jquery-1.11.1.min.js"></script>
+				<![endif]-->
+				<!--[if gte IE 9]><!-->
+					<script src="http://localhost/global/jquery/jquery-2.1.1.min.js"></script>
+				<!--<![endif]-->
 
-			<script src="http://localhost/global/bootstrap/3.1/js/bootstrap.min.js"></script>
-		@endif
-		@yield('scripts')
+				<script src="http://localhost/global/bootstrap/3.1/js/bootstrap.min.js"></script>
+			@endif
+			@yield('scripts')
+		</div>
 	</body>
 </html>
---
+++ Xtras/Web/views/pages/mobile.blade.php
@@ -0,0 +1,35 @@
+<header>
+	<div class="brand text-center">AnodyneXtras</div>
+</header>
+<section>
+	<div class="container">
+		<h1>We're So Sorry...</h1>
+
+		<p>Because of the nature of downloading and uploading content with AnodyneXtras, it is not available on mobile devices. If you want to browse, search, and upload, please use a laptop or desktop computer.</p>
+
+		<p>You can also check out some of our other sites and services using the links below.</p>
+
+		<hr>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-6">
+				<p><a href="http://anodyne-productions.com" class="btn btn-lg btn-block btn-default">Anodyne Productions</a></p>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<p><a href="http://anodyne-productions.com/nova" class="btn btn-lg btn-block btn-default">Nova</a></p>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<p><a href="http://forums.anodyne-productions.com" class="btn btn-lg btn-block btn-default">Anodyne Forums</a></p>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<p><a href="http://docs.anodyne-productions.com" class="btn btn-lg btn-block btn-default">AnodyneDocs</a></p>
+			</div>
+			<!--<div class="col-xs-12 col-sm-6">
+				<p><a href="#" class="btn btn-lg btn-block btn-default">Help Center</a></p>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<p><a href="#" class="btn btn-lg btn-block btn-default">Learning Center</a></p>
+			</div>-->
+		</div>
+	</div>
+</section>

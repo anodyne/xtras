ee7b8bb2d16ab8b12083239dc1c97608a9f7f08e
David VanScott <david.vanscott@gmail.com>
2014-05-24 23:52:48 -0400

    Working on the main page a little

--- Xtras/Web/views/pages/main.blade.php
+++ Xtras/Web/views/pages/main.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 	<div id="newest">
-		<a href="#" class="btn btn-sm btn-default pull-right">See Recently Updated Xtras</a>
+		<a href="#" data-target="updated" class="btn btn-sm btn-default pull-right xtra-toggle">See Recently Updated Xtras</a>
 
 		<h2>Newest Xtras</h2>
 
@@ -19,8 +19,8 @@
 		</div>
 	</div>
 	
-	<div id="updated">
-		<a href="#" class="btn btn-sm btn-default pull-right">See Newest Xtras</a>
+	<div id="updated" class="hide">
+		<a href="#" data-target="newest" class="btn btn-sm btn-default pull-right xtra-toggle">See Newest Xtras</a>
 
 		<h2>Recently Updated Xtras</h2>
 
@@ -32,4 +32,41 @@
 			@endforeach
 		</div>
 	</div>
+@stop
+
+@section('scripts')
+	{{ HTML::script('js/jquery.ui.effect.min.js') }}
+	<script>
+
+		$('.xtra-toggle').on('click', function(e)
+		{
+			e.preventDefault();
+
+			var target = $(this).data('target');
+
+			if (target == 'newest')
+			{
+				$('#updated').addClass('hide', {
+					duration: 500,
+					queue: false
+				});
+				$('#newest').removeClass('hide', {
+					duration: 500,
+					queue: false
+				});
+			}
+			else if (target == 'updated')
+			{
+				$('#newest').addClass('hide', {
+					duration: 500,
+					queue: false
+				});
+				$('#updated').removeClass('hide', {
+					duration: 500,
+					queue: false
+				});
+			}
+		});
+
+	</script>
 @stop

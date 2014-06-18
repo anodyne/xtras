4403d3f68666cba760beafd73df1c6162a21d4c3
David VanScott <david.vanscott@gmail.com>
2014-06-18 15:23:09 -0400

    More item seeder work

--- Xtras/database/seeds/ItemSeeder.php
+++ Xtras/database/seeds/ItemSeeder.php
@@ -13,24 +13,24 @@ class ItemSeeder extends Seeder {
 
 		$items = array(
 			array(
-				'user_id' => 1,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 1,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'Pulsar',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 1,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 1,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'Titan',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 1,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 1,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'Lightness',
 				'desc' => $faker->paragraph(10),
@@ -38,32 +38,32 @@ class ItemSeeder extends Seeder {
 			),
 
 			array(
-				'user_id' => 2,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 2,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'DS9 Duty Uniform',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 2,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 2,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'DS9 Dress Uniform',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 2,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 2,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'DS9 Alternate Uniform #1',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 2,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 2,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'DS9 Alternate Uniform #2',
 				'desc' => $faker->paragraph(10),
@@ -71,24 +71,24 @@ class ItemSeeder extends Seeder {
 			),
 			
 			array(
-				'user_id' => 1,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 3,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'Skinny Bio Page',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 1,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 3,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'External Post Archiving',
 				'desc' => $faker->paragraph(10),
 				'slug' => '',
 			),
 			array(
-				'user_id' => 1,
-				'type_id' => $faker->numberBetween(1, 3),
+				'user_id' => $faker->numberBetween(1, 2),
+				'type_id' => 3,
 				'product_id' => $faker->numberBetween(1, 3),
 				'name' => 'Stardate Display',
 				'desc' => $faker->paragraph(10),
@@ -104,7 +104,7 @@ class ItemSeeder extends Seeder {
 		for ($i = 0; $i < 20; $i++)
 		{
 			ItemRatingModel::create(array(
-				'user_id' => 1,
+				'user_id' => $faker->numberBetween(1, 2),
 				'item_id' => 1,
 				'rating' => $faker->numberBetween(1, 5),
 			));

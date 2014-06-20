0163526f6e3eb8dbc1435bc9948aa6d04ac77fe6
David VanScott <david.vanscott@gmail.com>
2014-06-20 13:15:29 -0400

    More work on creating an xtra

--- Xtras/Web/views/pages/item/create.blade.php
+++ Xtras/Web/views/pages/item/create.blade.php
@@ -11,7 +11,7 @@
 
 	<hr>
 
-	{{ Form::open(['route' => 'xtra.store']) }}
+	{{ Form::open(['route' => 'item.store']) }}
 		<div class="row">
 			<div class="col-lg-3">
 				<div class="form-group">
@@ -33,6 +33,15 @@
 				<div class="form-group">
 					<label>Name</label>
 					{{ Form::text('name', null, ['class' => 'form-control']) }}
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-2">
+				<div class="form-group">
+					<label>Version</label>
+					{{ Form::text('version', '1.0', ['class' => 'form-control']) }}
 				</div>
 			</div>
 		</div>
@@ -61,7 +70,17 @@
 				<div class="form-group">
 					<label>Installation Instructions</label>
 					{{ Form::textarea('meta[installation]', null, ['class' => 'form-control', 'rows' => 8]) }}
-					<p class="help-block">You can use Markdown to style your installation instructions.</p>
+					<p class="help-block text-sm">{{ $_icons['markdown'] }} Parsed as Markdown</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="form-group">
+					<label>Version History</label>
+					{{ Form::textarea('meta[history]', null, ['class' => 'form-control', 'rows' => 8]) }}
+					<p class="help-block text-sm">{{ $_icons['markdown'] }} Parsed as Markdown</p>
 				</div>
 			</div>
 		</div>
@@ -74,8 +93,36 @@
 				{{ Form::button('Create and Continue', ['type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary']) }}
 			</div>
 			<div class="col-lg-9">
-				<button class="btn btn-lg btn-link" disabled="disabled">Next Step: Upload Files and Preview Images</button>
+				<button class="btn btn-lg btn-link" disabled="disabled">Next Step: Upload Item Zip File</button>
 			</div>
 		</div>
 	{{ Form::close() }}
+@stop
+
+@section('scripts')
+
+	<script>
+
+		$('[name="name"]').on('change', function()
+		{
+			var value = $(this).val();
+
+			$.ajax({
+				url: "{{ URL::route('item.ajax.checkName') }}",
+				type: "POST",
+				dataType: "json",
+				data: { name: value },
+				success: function(data)
+				{
+					if (data.code == 0)
+					{
+						alert("You already have an xtra with that name. Please choose a different name!");
+						$('[name="name"]').val('');
+					}
+				}
+			});
+		});
+
+	</script>
+
 @stop

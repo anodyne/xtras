f2d2dd0d75fe810c5bba27e5c5f0d19f11c3e241
David VanScott <david.vanscott@gmail.com>
2014-06-20 13:15:00 -0400

    Fix some route change errors

--- Xtras/Web/views/partials/media.blade.php
+++ Xtras/Web/views/partials/media.blade.php
@@ -1,7 +1,7 @@
 <div class="xtra">
 	{{ View::make('partials.avatar')->withType(false)->withUrl('http://www.hdwallpapers.in/walls/fantasy_space-wide.jpg')->withClass('item-preview') }}
 
-	{{ $item->user->present()->avatar(['type' => 'link', 'link' => URL::route('profile', [$item->user->slug]), 'class' => 'avatar xtra-avatar img-circle']) }}
+	{{ $item->user->present()->avatar(['type' => 'link', 'link' => URL::route('account.profile', [$item->user->slug]), 'class' => 'avatar xtra-avatar img-circle']) }}
 
 	<h4 class="xtra-heading">{{ $item->present()->name }}</h4>
 	<div class="text-center">
@@ -9,6 +9,6 @@
 		{{ $item->present()->productAsLabel }}
 	</div>
 	<div class="xtra-desc">
-		<p><a href="{{ URL::route('item', [$item->user->slug, $item->slug]) }}" class="btn btn-lg btn-block btn-default">More Info</a></p>
+		<p><a href="{{ URL::route('item.show', [$item->user->slug, $item->slug]) }}" class="btn btn-lg btn-block btn-default">More Info</a></p>
 	</div>
 </div>

300e81534ca784a13c8f28f468fcf1f9e128c86b
David VanScott <david.vanscott@gmail.com>
2014-06-22 12:14:30 -0400

    Add modal stuff

--- app/helpers.php
+++ app/helpers.php
@@ -14,3 +14,15 @@ if ( ! function_exists('partial'))
 		return $view;
 	}
 }
+
+if ( ! function_exists('modal'))
+{
+	function modal(array $data = array())
+	{
+		return View::make('partials.modal')
+			->with('modalId', (array_key_exists('id', $data)) ? $data['id'] : false)
+			->with('modalHeader', (array_key_exists('header', $data)) ? $data['header'] : false)
+			->with('modalBody', (array_key_exists('body', $data)) ? $data['body'] : false)
+			->with('modalFooter', (array_key_exists('footer', $data)) ? $data['footer'] : false);
+	}
+}

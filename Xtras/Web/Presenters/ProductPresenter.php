8f55293bd3e822d829be80ed05853b99c9830a16
David VanScott <david.vanscott@gmail.com>
2014-06-23 14:10:10 -0400

    Add the product presenter

---
+++ Xtras/Web/Presenters/ProductPresenter.php
@@ -0,0 +1,23 @@
+<?php namespace Xtras\Presenters;
+
+use Str, Markdown;
+use Laracasts\Presenter\Presenter;
+
+class ProductPresenter extends Presenter {
+
+	public function desc()
+	{
+		return Markdown::parse($this->entity->desc);
+	}
+
+	public function name()
+	{
+		return $this->entity->name;
+	}
+
+	public function nameAsSlug()
+	{
+		return Str::slug(Str::lower($this->entity->name));
+	}
+
+}

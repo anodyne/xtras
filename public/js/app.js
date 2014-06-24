961e5b952aee583ac99d65cb7a39b9d9d8932dd9
David VanScott <david.vanscott@gmail.com>
2014-06-24 14:37:14 -0400

    Add app js file

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+var xtrasApp = angular.module('xtrasApp', [], function($interpolateProvider) {
+	$interpolateProvider.startSymbol('<%');
+	$interpolateProvider.endSymbol('%>');
+});
+
+function CommentsController($scope, $http, $window) {
+
+	$http.get($window.url + '/comments/' + $window.itemId).success(function(comments)
+	{
+		$scope.comments = comments;
+	});
+
+	$scope.countComments = function()
+	{
+		var count = 0;
+
+		angular.forEach($scope.comments, function(comment)
+		{
+			count += 1;
+		});
+
+		return count;
+	}
+
+	$scope.addComment = function()
+	{
+		var comment = {
+			user_id: $window.userId,
+			item_id: $window.itemId,
+			content: $scope.newCommentContent
+		};
+
+		// Push the content onto the comments
+		$scope.comments.unshift(comment);
+
+		// Post the data
+		$http.post($window.url + '/comments/' + $window.itemId, comment);
+
+		// Hide the panel
+		$('#commentPanel').addClass('hide');
+	}
+}

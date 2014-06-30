4e6132d38ff52a90c8c63fe9c83377bb1ce5beed
David VanScott <david.vanscott@gmail.com>
2014-06-29 20:37:44 -0400

    View cleanup

--- Xtras/Web/views/pages/error.blade.php
+++ Xtras/Web/views/pages/error.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
 @section('title')
-	Error!
+	Error
 @stop
 
 @section('content')
 	<h1 class="text-{{ $type }}">Error!</h1>
 
-	{{ partial('alert', ['type' => $type, 'content' => $error]) }}
+	<p class="alert alert-{{ $type }}">{{ $error }}</p>
 @stop
--- Xtras/Web/views/pages/register.blade.php
+++ Xtras/Web/views/pages/register.blade.php
@@ -5,30 +5,51 @@
 @stop
 
 @section('content')
-	<h1>Register</h1>
+	<div class="row">
+		<div class="col-md-8 col-lg-6 col-md-offset-2 col-lg-offset-3">
+			<h1>Register</h1>
 
-	<div class="row hide" id="registerForm">
-		<div class="col-lg-6 col-offset-3">
-			{{ Form::open(array('url' => 'register')) }}
+			<p>AnodyneXtras is the one-stop-shop to find skins, MODs and rank sets created by Anodyne Productions and the wider Anodyne community. In order to share, search, and download xtras for you game, you'll need to first take a few seconds to register.</p>
+
+			<hr>
+
+			{{ Form::open(['route' => 'register.do']) }}
 				<div class="form-group">
 					<label>Name</label>
-					{{ Form::text('name', null, array('class' => 'form-control input-with-feedback')) }}
+					{{ Form::text('name', null, ['class' => 'form-control input-with-feedback']) }}
+					<p class="help-block">Choose wisely! This is the name that will be used as your profile name and to associate all your xtras with your account.</p>
+					{{ $errors->first('name', '<p class="help-block">:message</p>') }}
 				</div>
 
 				<div class="form-group">
 					<label>Email Address</label>
-					{{ Form::text('email', null, array('type' => 'email', 'class' => 'form-control input-with-feedback')) }}
+					{{ Form::text('email', null, ['type' => 'email', 'class' => 'form-control input-with-feedback']) }}
+					{{ $errors->first('email', '<p class="help-block">:message</p>') }}
 				</div>
 
 				<div class="form-group">
 					<label>Password</label>
-					{{ Form::password('password', array('class' => 'form-control input-with-feedback')) }}
+					{{ Form::password('password', ['class' => 'form-control input-with-feedback']) }}
+					{{ $errors->first('password', '<p class="help-block">:message</p>') }}
 				</div>
 
 				<div class="form-group">
-					{{ Form::submit('Register', array('class' => 'btn btn-large btn-block btn-primary')) }}
+					<label>Confirm Password</label>
+					{{ Form::password('password_confirm', ['class' => 'form-control input-with-feedback']) }}
+					{{ $errors->first('password_confirm', '<p class="help-block">:message</p>') }}
+				</div>
 
-					<a href="#" class="btn btn-block btn-default js-switch-login">Log In Now</a>
+				<div class="form-group">
+					<label>Spam-Be-Gone</label>
+					<p class="help-block">In order to prevent spam, please type in the following number to the field below: <strong>{{ $confirmNumber }}</strong>.</p>
+					{{ Form::text('confirm', null, ['class' => 'form-control input-with-feedback']) }}
+					{{ $errors->first('confirm', '<p class="help-block">:message</p>') }}
+				</div>
+
+				<div class="form-group">
+					{{ Form::button('Register', ['type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary']) }}
+
+					<a href="{{ route('login') }}" class="btn btn-block btn-link">Log In Now</a>
 				</div>
 			{{ Form::close() }}
 		</div>

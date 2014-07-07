a0e4c943b7d2cab3338f3df04afb862596bf9d0c
David VanScott <david.vanscott@gmail.com>
2014-07-07 15:49:30 -0400

    Add some basic navigation to admin page

--- Xtras/Web/views/pages/admin/index.blade.php
+++ Xtras/Web/views/pages/admin/index.blade.php
@@ -6,4 +6,25 @@
 
 @section('content')
 	<h1>Admin</h1>
+
+	<div class="row">
+		<div class="col-md-9">
+			
+		</div>
+
+		<div class="col-md-3">
+			<div class="list-group">
+				<a href="{{ route('admin.users.index') }}" class="list-group-item">
+					<span class="badge">15</span>
+					Users
+				</a>
+				<a href="{{ route('admin.items.index') }}" class="list-group-item">
+					<span class="badge">1,023</span>
+					Items
+				</a>
+				<a href="{{ route('admin.products.index') }}" class="list-group-item">Products</a>
+				<a href="{{ route('admin.types.index') }}" class="list-group-item">Item Types</a>
+			</div>
+		</div>
+	</div>
 @stop

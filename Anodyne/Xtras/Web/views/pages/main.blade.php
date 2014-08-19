4257c22cdf9e996ef48c1fd226006395d035b6e6
David VanScott <david.vanscott@gmail.com>
2014-08-19 17:03:26 -0400

    New button style

--- Anodyne/Xtras/Web/views/pages/main.blade.php
+++ Anodyne/Xtras/Web/views/pages/main.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 	<div id="newest">
-		<a href="#" data-target="updated" class="btn btn-sm btn-default pull-right xtra-toggle">See Recently Updated Xtras</a>
+		<a href="#" data-target="updated" class="btn btn-sm btn-primary-alt pull-right xtra-toggle">See Recently Updated Xtras</a>
 
 		<h1>Newest Xtras</h1>
 
@@ -20,7 +20,7 @@
 	</div>
 		
 	<div id="updated" class="hide">
-		<a href="#" data-target="newest" class="btn btn-sm btn-default pull-right xtra-toggle">See Newest Xtras</a>
+		<a href="#" data-target="newest" class="btn btn-sm btn-primary-alt pull-right xtra-toggle">See Newest Xtras</a>
 
 		<h1>Recently Updated Xtras</h1>
 

0b6f8a5a99d6e5a3651985d3dc87e9b382242aac
David VanScott <david.vanscott@gmail.com>
2014-08-19 17:02:22 -0400

    Login page changes

--- Anodyne/Xtras/Web/views/pages/login.blade.php
+++ Anodyne/Xtras/Web/views/pages/login.blade.php
@@ -13,22 +13,23 @@
 
 			<hr>
 
-			{{ Form::open(array('url' => 'login')) }}
+			{{ Form::open(['url' => 'login']) }}
 				<div class="form-group{{ ($errors->has('email')) ? ' has-error' : '' }}">
 					<label>Email Address</label>
-					{{ Form::text('email', null, array('type' => 'email', 'class' => 'form-control input-with-feedback input-lg')) }}
+					{{ Form::text('email', null, ['type' => 'email', 'class' => 'form-control input-with-feedback input-lg']) }}
 					{{ $errors->first('email', '<p class="help-block">:message</p>') }}
 				</div>
 
 				<div class="form-group{{ ($errors->has('password')) ? ' has-error' : '' }}">
 					<label>Password</label>
-					{{ Form::password('password', array('class' => 'form-control input-with-feedback input-lg')) }}
+					{{ Form::password('password', ['class' => 'form-control input-with-feedback input-lg']) }}
 					{{ $errors->first('password', '<p class="help-block">:message</p>') }}
 				</div>
 
 				<div class="form-group">
 					{{ Form::button('Log In', ['type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary']) }}
-					<a href="{{ route('register') }}" class="btn btn-block btn-link">Register Now</a>
+					<a href="http://anodyne-productions.com/register" class="btn btn-block btn-link">Register Now</a>
+					<a href="http://anodyne-productions.com/password/remind" class="btn btn-block btn-link">Forgot Password?</a>
 				</div>
 			{{ Form::close() }}
 		</div>

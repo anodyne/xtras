29386f77d887398bf4404f6a30e4be89ae950d2c
David VanScott <david.vanscott@gmail.com>
2014-08-30 13:27:02 -0400

    Force data to be passed to create and update

--- Anodyne/Xtras/Foundation/Data/Interfaces/BaseRepositoryInterface.php
+++ Anodyne/Xtras/Foundation/Data/Interfaces/BaseRepositoryInterface.php
@@ -3,9 +3,9 @@
 interface BaseRepositoryInterface {
 	
 	public function all();
-	public function create(array $data = []);
+	public function create(array $data);
 	public function delete($id);
 	public function find($id);
-	public function update($id, array $data = []);
+	public function update($id, array $data);
 
 }

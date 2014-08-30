3a5da7ded0a4e6251dad2038925bd5b762cf6950
David VanScott <david.vanscott@gmail.com>
2014-08-30 13:27:48 -0400

    Remove some views

--- Anodyne/Xtras/Web/views/pages/user/create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('title')
-	Create Account
-@stop
-
-@section('content')
-	<h1>Create Account</h1>
-@stop
--- Anodyne/Xtras/Web/views/pages/user/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('title')
-	Accounts
-@stop
-
-@section('content')
-	<h1>All Accounts</h1>
-@stop

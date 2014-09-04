57337353f7873e2c64e355de9d810fe60c14fb9d
David VanScott <david.vanscott@gmail.com>
2014-09-03 20:50:23 -0400

    Cleanup the routes file

--- app/routes.php
+++ app/routes.php
@@ -3,28 +3,3 @@
 /**
  * Routes for the system are stored in the XtrasRoutingServiceProvider.
  */
-
-Route::get('email', function()
-{
-	// Get the item
-	$item = ItemModel::first();
-
-	if ($item)
-	{
-		// Get the user it's coming from
-		$user = UserModel::find(5);
-
-		$emailData = [
-			'subject' => "Issue Reported - ".$item->present()->name,
-			'content' => "Content",
-			'from' => $user->present()->email,
-			'to' => $item->user->present()->email,
-			'name' => HTML::link(route('item.show', [$item->user->username, $item->slug]), $item->present()->name),
-			'type' => $item->present()->type,
-			'userName' => $user->present()->name,
-			'userEmail' => $user->present()->email,
-		];
-
-		return View::make('emails.issue')->with($emailData);
-	}
-});

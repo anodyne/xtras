2d47d0970429c2537f985226c12d9a860d26a22e
David VanScott <david.vanscott@gmail.com>
2014-09-05 13:59:56 -0400

    Clean up some of the minor pages

--- Anodyne/Xtras/Web/views/pages/faq.blade.php
+++ Anodyne/Xtras/Web/views/pages/faq.blade.php
@@ -8,25 +8,23 @@
 	<h1>Frequently Asked Questions</h1>
 
 	<dl>
-		<dt>Why do I need to register to view and download xtras?</dt>
-		<dd>In order to provide notification services when an xtra is updated, you need to have an account.</dd>
-
-		<dt>Why do I keep getting emails telling me an xtra has been updated? How do I stop the emails?</dt>
+		<dt>Why do I need to register to download Xtras?</dt>
+		<dd>In order to provide notification services when an Xtra is updated, you need to have an account. This also allows you to have a complete history of your download history.</dd>
 	</dl>
 
 	<h2>Creating, Updating, &amp; Deleting Xtras</h2>
 
 	<dl>
-		<dt>Can I have an xtra that targets multiple versions of Nova?</dt>
-		<dd>No. We considered allowing this, but in the end, we felt it would've created more confusion if a user went to the same place to get different versions of an xtra. It would be easy to accidentally get the wrong version. By forcing xtras to have separate versions for each version of Nova, everything from preview images, version history, installation instructions, and downloads are clearer.</dd>
+		<dt>Can I have an Xtra that targets multiple versions of Nova?</dt>
+		<dd>No. We considered allowing this, but in the end, we felt it would've created more confusion if a user went to the same place to get different versions of an Xtra. It would be easy to accidentally get the wrong version. By forcing Xtras to have separate versions for each version of Nova, everything from preview images, version history, installation instructions, and downloads are clearer.</dd>
 
-		<dt>Can I have more than 3 preview images for my xtra?</dt>
+		<dt>Can I have more than 3 preview images for my Xtra?</dt>
 		<dd>No. We have decided on allowing 3 preview images at this time.</dd>
 
 		<dt>Why can't I create a rank set Xtra?</dt>
-		<dd>Because of how complicated the rank system is in Nova 1 and 2, there are a limited number of users with access to create rank set xtras. In the future, when Nova 3 is released, we'll open it up to more people. If you have a rank set you'd like to share, contact us and we'll consider providing you access to create a new rank set xtra.</dd>
+		<dd>Because of how complicated the rank system is in Nova 1 and 2, there are a limited number of users with access to create rank set Xtras. In the future, when Nova 3 is released, we'll open it up to more people. If you have a rank set you'd like to share, contact us and we'll consider providing you access to create a new rank set Xtra.</dd>
 
 		<dt>How do I update one of my Xtras with a new version?</dt>
-		<dd>If you want to add a new version to one of your Xtras, it's as simple as editing the Xtra. You'll update the basic information and then step through the same process you did for creating the Xtra to upload your zip file and update any preview images.</dd>
+		<dd>If you want to add a new version to one of your Xtras, it's as simple as editing the Xtra. You'll update the basic information (including the version number) and then use the File Management page to add a new file for the version.</dd>
 	</dl>
 @stop
--- Anodyne/Xtras/Web/views/pages/policies/terms.blade.php
+++ Anodyne/Xtras/Web/views/pages/policies/terms.blade.php
@@ -15,7 +15,7 @@
 
 	<dl>
 		<dt>"As is/available"</dt>
-		<dd>We've worked hard to build this Service, but we're human. There are bound to be bugs, snafus, and weird issues along the way. We apologize in advance for those, but we're providing this Service free of charge and on an "as is" and "as available" basis. If you let us know about an issue you're having, we'll gladly look to address it as quickly as possible.</dd>
+		<dd>We've worked hard to build AnodyneXtras, but we're human. There are bound to be issues, snafus, and weird things along the way. We apologize in advance for those, but we're providing this service free of charge and on an "as is" and "as available" basis. If you let us know about an issue you're having, we'll gladly look to address it as quickly as possible.</dd>
 
 		<dt>No Breaking the Law</dt>
 		<dd>Simply put, if it's illegal or unauthorized where you live, don't do it here either.</dd>
@@ -25,22 +25,22 @@
 
 		<dt>It Belongs to YOU</dt>
 		<dd>Your account is your account. That means you (and you alone) are responsible for maintaining the security of your account.</dd>
-		<dd>You are responsible for all activity and Content posted from your account.</dd>
-		<dd>Even though we're hosting it, your Content will always be your Content. By uploading it to the Service, you're agreeing to allow others to view and use the Content. If you don't want other people using it, don't upload it.</dd>
+		<dd>You are responsible for all activity and content posted from your account.</dd>
+		<dd>Even though we're hosting it, your content will always be your content. By uploading it to AnodyneXtras, you're agreeing to allow others to view and use your content. If you don't want other people using it, don't upload it.</dd>
 
 		<dt>Don't Be a Jerk</dt>
-		<dd>Don't put worms, viruses, trojans, or any code of a destructive or malicious nature into your Content.</dd>
-		<dd>Don't use the Service to transmit any unsolicited ("spam") messages to users of the Service.</dd>
-		<dd>Don't steal other people's hard work, whether it's Content from within the Service or outside of it.</dd>
+		<dd>Don't put worms, viruses, trojans, or any code of a destructive or malicious nature into your Xtras.</dd>
+		<dd>Don't use AnodyneXtras to transmit any unsolicited ("spam") messages to users.</dd>
+		<dd>Don't steal other people's hard work, whether it's content from within AnodyneXtras or outside of it.</dd>
 
 		<dt>Don't Hog All the Space</dt>
-		<dd>Anodyne pays for the space used for storing all of the Content available through the Service. If you're using too much space, we reserve the right to disable your account or suspend your ability to upload to the Service until you can reduce how much space you're using.</dd>
+		<dd>Anodyne Productions pays for the space used for storing all of the content available on AnodyneXtras. If you're using too much space, we reserve the right to disable your account or suspend your ability to upload until you can reduce how much space you're using.</dd>
 
 		<dt>Removal of Content</dt>
-		<dd>We don't pre-screen any Content before it's available on the Service.</dd>
-		<dd>We may, though have no obligation to, remove Content and Accounts containing Content that we determine in our sole discretion to be unlawful, offensive, threatening, libelous, defamatory, pornographic, obscene or otherwise objectionable or that violates someone else's intellectual property.</dd>
+		<dd>We don't pre-screen any Xtras before they're available.</dd>
+		<dd>We may (though have no obligation to) remove Xtras and accounts containing content that we determine in our sole discretion to be unlawful, offensive, threatening, libelous, defamatory, pornographic, obscene or otherwise objectionable or that violates someone else's intellectual property.</dd>
 
 		<dt>Be Nice</dt>
-		<dd>Verbal, physical, written or other abuse (including threats of abuse or retribution) of any Anodyne customer, employee, member, or officer will result in immediate account termination.</dd>
+		<dd>Verbal, physical, written or other abuse (including threats of abuse or retribution) of any Anodyne Production customer or employee will result in immediate account termination.</dd>
 	</dl>
 @stop

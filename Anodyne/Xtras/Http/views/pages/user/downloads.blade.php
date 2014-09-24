c38e3f120d6daa96baf5a1e2b3f452b9994596e6
David VanScott <david.vanscott@gmail.com>
2014-09-24 11:33:00 -0400

    Add intro and download button to My Downloads

--- Anodyne/Xtras/Http/views/pages/user/downloads.blade.php
+++ Anodyne/Xtras/Http/views/pages/user/downloads.blade.php
@@ -8,6 +8,8 @@
 	<h1>My Downloads</h1>
 
 	@if ($orders->count() > 0)
+		<p>Your download history is available from here and will show everything you've downloaded since registering for AnodyneXtras. You can view the Xtras you've downloaded or quickly re-download the Xtras if you need to.</p>
+
 		<div class="data-table data-table-bordered data-table-striped">
 		@foreach ($orders as $o)
 			<div class="row">
@@ -21,6 +23,9 @@
 					<div class="btn-toolbar pull-right">
 						<div class="btn-group">
 							<a href="{{ route('item.show', [$o->item->user->username, $o->item->slug]) }}" class="btn btn-default">View</a>
+						</div>
+						<div class="btn-group">
+							<a href="{{ route('item.download', [$o->item->user->username, $o->item->slug, $o->file->id]) }}" class="btn btn-default">Download</a>
 						</div>
 					</div>
 				</div>

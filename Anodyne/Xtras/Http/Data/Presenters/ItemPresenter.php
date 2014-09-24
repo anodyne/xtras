ccfb82fb6300a7410dca628d5997c0740cf062ed
David VanScott <david.vanscott@gmail.com>
2014-09-24 11:32:26 -0400

    Fix issue with download button

--- Anodyne/Xtras/Http/Data/Presenters/ItemPresenter.php
+++ Anodyne/Xtras/Http/Data/Presenters/ItemPresenter.php
@@ -69,7 +69,13 @@ class ItemPresenter extends Presenter {
 		// Get the latest version
 		$latest = $this->entity->getLatestVersion()->toArray();
 
-		$link = route('item.download', [$this->entity->id, $latest['files']['id']]);
+		$downloadOptions = [
+			$this->entity->user->username,
+			$this->entity->slug,
+			$latest['files']['id']
+		];
+
+		$link = route('item.download', $downloadOptions);
 		$title = '<span class="tab-icon tab-icon-up2 tab-icon-right">'.$downloadIcon.'</span>';
 		$title.= '<span class="visible-md">Download</span>';
 		$title.= '<span class="visible-lg">Download Latest Version</span>';

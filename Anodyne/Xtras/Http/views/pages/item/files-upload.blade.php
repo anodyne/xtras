6769710414b11a69ea34056437379946c2992927
David VanScott <david.vanscott@gmail.com>
2014-09-29 13:40:33 -0400

    Clean up the files view

--- Anodyne/Xtras/Http/views/pages/item/files-upload.blade.php
+++ Anodyne/Xtras/Http/views/pages/item/files-upload.blade.php
@@ -26,22 +26,22 @@
 			<li>Drag-and-drop your zip file to the area below.</li>
 		</ol>
 
-		<p class="alert"><strong>Please note:</strong> While there are no file size limitations imposed, please try to keep your files to a reasonable size. If you're found to be uploading abnormally large files, limits could be imposed on your account or even suspended. If you have questions about acceptable file sizes and if your files meet that definition, contact us first.</p>
+		<p class="alert alert-warning"><strong>Please note:</strong> While there are no file size limitations imposed, please try to keep your files to a reasonable size. If you're found to be uploading abnormally large files, limits could be imposed on your account or even suspended. If you have questions about acceptable file sizes and if your files meet that definition, contact us first.</p>
 
 		<p><div id="dzZip" class="dropzone"></div></p>
 
 		<div class="hide" id="buttons">
 			<div class="btn-toolbar">
 				<div class="btn-group">
-					<a href="{{ route('item.images.index', [$item->user->username, $item->slug]) }}" class="btn btn-lg btn-primary"><span class="tab-icon tab-icon-right">{{ $_icons['images'] }}</span>Manage Xtra Images</a>
+					<a href="{{ route('item.images.index', [$item->user->username, $item->slug]) }}" class="btn btn-lg btn-primary">Manage Xtra Images</a>
 				</div>
 				<div class="btn-group">
-					<a href="{{ route('account.xtras', [$item->user->username, $item->slug]) }}" class="btn btn-lg btn-default"><span class="tab-icon tab-icon-right">{{ $_icons['user'] }}</span>My Xtras</a>
+					<a href="{{ route('account.xtras') }}" class="btn btn-lg btn-default">My Xtras</a>
 				</div>
 			</div>
 		</div>
 	@else
-		<p class="alert alert-warning">You already have a file associated with this version of your Xtra. In order to upload a new file, you need to <a href="{{ route('item.edit', [$item->user->username, $item->slug]) }}">update the version info</a> on your Xtra and then come back to upload your zip file.</p>
+		{{ alert('warning', "You already have a file associated with this version of your Xtra. In order to upload a new file, you need to ".link_to_route('item.edit', 'update the version info', [$item->user->username, $item->slug])." on your Xtra and then come back to upload your zip file.") }}
 	@endif
 @stop
 

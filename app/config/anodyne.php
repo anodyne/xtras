783adfe16fcdf6a4fe3efe4d018002c7b9d07e7a
David VanScott <david.vanscott@gmail.com>
2014-11-04 16:05:58 -0500

    Move the config files around

---
+++ app/config/anodyne.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+	'email' => [
+
+		'abuse'		=> 'admin@anodyne-productions.com',
+		'general'	=> 'xtras@anodyne-productions.com',
+
+		'subject'	=> "[AnodyneXtras]",
+
+	],
+
+	'links' => [
+
+		'www'		=> 'http://anodyne-productions.com/',
+		'nova'		=> 'http://anodyne-productions.com/nova/',
+		'forums'	=> 'http://forums.anodyne-productions.com/',
+		'help'		=> 'http://help.anodyne-productions.com/',
+
+	],
+
+	'usageWarning' => 250000000,
+
+];

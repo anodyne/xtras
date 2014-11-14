2515d0dee70e2ce5372bc62c7ea0d3d1358d9417
David VanScott <david.vanscott@gmail.com>
2014-11-13 22:16:43 -0500

    Tweak how preview images are displayed

--- Anodyne/Xtras/views/partials/image.blade.php
+++ Anodyne/Xtras/views/partials/image.blade.php
@@ -1,5 +1,5 @@
 @if ($type == 'link')
-	<a href="{{ $link }}" class="{{ $class }}" style="background-image:url({{ $url }})"></a>
+	<a href="{{ $link }}" class="{{ $class }}" style="background-image:url({{ $url }});background-position:top center;"></a>
 @else
-	<div class="{{ $class }}" style="background-image:url({{ $url }})"></div>
+	<div class="{{ $class }}" style="background-image:url({{ $url }});background-position:top center;"></div>
 @endif
